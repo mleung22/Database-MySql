c7fb66424e2486c40a84350c565ae74e0386ff9a
Mike Leung <mleung22@gmail.com>
2015-05-04 15:46:31 -0400

    JavaFX Main

--- Main.java
+++ Main.java
@@ -1,19 +1,27 @@
 package application;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -22,7 +30,7 @@ public class Main extends Application {
 
 	public void start(Stage primaryStage) {
 		try {
-			Database db = new Database();
+			Database database = new Database();
 
 			primaryStage.setTitle("Database Manipulation");
 
@@ -34,43 +42,32 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 
 			Text scenetitle = new Text("Database Manipulation");
-			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-			grid.add(scenetitle, 3, 0);
-
 			Button insertDataBtn = new Button("Insert Data into Database");
-			HBox insertHB = new HBox();
-			insertHB.setAlignment(Pos.CENTER);
-			insertHB.getChildren().add(insertDataBtn);
-			grid.add(insertHB, 3, 1);
-
 			Button deleteDataBtn = new Button("Delete Data from Database");
-			HBox deleteHB = new HBox();
-			deleteHB.setAlignment(Pos.CENTER);
-			deleteHB.getChildren().add(deleteDataBtn);
-			grid.add(deleteHB, 3, 2);
-
-			Button searchAllBtn = new Button("Print out database to console");
-			HBox searchHB = new HBox();
-			searchHB.setAlignment(Pos.CENTER);
-			searchHB.getChildren().add(searchAllBtn);
-			grid.add(searchHB, 3, 3);
-
+			Button searchAllBtn = new Button("View Database");
 			Button findPersonBtn = new Button(
 					"Find Person in Database by Last Name");
-			HBox findPersonLastNameHB = new HBox();
-			findPersonLastNameHB.setAlignment(Pos.CENTER);
-			findPersonLastNameHB.getChildren().add(findPersonBtn);
-			grid.add(findPersonLastNameHB, 3, 4);
+
+			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+
+			VBox mainGridButtonsVBox = new VBox();
+			mainGridButtonsVBox.setAlignment(Pos.CENTER);
+			mainGridButtonsVBox.setSpacing(5);
+			mainGridButtonsVBox.getChildren().addAll(insertDataBtn,
+					deleteDataBtn, searchAllBtn, findPersonBtn);
+
+			grid.add(scenetitle, 3, 0);
+			grid.add(mainGridButtonsVBox, 3, 1);
 
 			insertDataBtn.setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent e) {
+
 					Stage insertDataStage = new Stage();
 					insertDataStage.setTitle("Data Insertion");
 
 					GridPane insertDataGrid = new GridPane();
 					insertDataGrid.setAlignment(Pos.CENTER);
-					insertDataGrid.setHgap(5.0);
-					insertDataGrid.setVgap(5.0);
+					insertDataGrid.setVgap(2);
 
 					Scene insertDataScene = new Scene(insertDataGrid, 300, 250);
 					insertDataStage.setScene(insertDataScene);
@@ -78,76 +75,39 @@ public class Main extends Application {
 					insertDataStage.show();
 
 					Label firstNameLabel = new Label("First Name:");
-					HBox firstNameLabelHB = new HBox();
-					firstNameLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					firstNameLabelHB.getChildren().add(firstNameLabel);
-					insertDataGrid.add(firstNameLabelHB, 3, 1);
-
-					TextField firstNameText = new TextField();
-					HBox firstNameTextHB = new HBox();
-					firstNameTextHB.setAlignment(Pos.CENTER_LEFT);
-					firstNameTextHB.getChildren().add(firstNameText);
-					insertDataGrid.add(firstNameTextHB, 4, 1);
-
 					Label lastNameLabel = new Label("Last Name:");
-					HBox lastNameLabelHB = new HBox();
-					lastNameLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					lastNameLabelHB.getChildren().add(lastNameLabel);
-					insertDataGrid.add(lastNameLabelHB, 3, 2);
-
-					TextField lastNameText = new TextField();
-					HBox lastNameTextHB = new HBox();
-					lastNameTextHB.setAlignment(Pos.CENTER_LEFT);
-					lastNameTextHB.getChildren().add(lastNameText);
-					insertDataGrid.add(lastNameTextHB, 4, 2);
-
 					Label ageLabel = new Label("Age:");
-					HBox ageLabelHB = new HBox();
-					ageLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					ageLabelHB.getChildren().add(ageLabel);
-					insertDataGrid.add(ageLabelHB, 3, 3);
-
-					TextField ageText = new TextField();
-					HBox ageTextHB = new HBox();
-					ageTextHB.setAlignment(Pos.CENTER_LEFT);
-					ageTextHB.getChildren().add(ageText);
-					insertDataGrid.add(ageTextHB, 4, 3);
-
 					Label ssnLabel = new Label("Social Security Number:");
-					HBox ssnLabelHB = new HBox();
-					ssnLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					ssnLabelHB.getChildren().add(ssnLabel);
-					insertDataGrid.add(ssnLabelHB, 3, 4);
-
-					TextField ssnText = new TextField();
-					HBox ssnTextHB = new HBox();
-					ssnTextHB.setAlignment(Pos.CENTER_LEFT);
-					ssnTextHB.getChildren().add(ssnText);
-					insertDataGrid.add(ssnTextHB, 4, 4);
-
 					Label ccLabel = new Label("Credit Card:");
-					HBox ccLabelHB = new HBox();
-					ccLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					ccLabelHB.getChildren().add(ccLabel);
-					insertDataGrid.add(ccLabelHB, 3, 5);
-
+					TextField firstNameText = new TextField();
+					TextField lastNameText = new TextField();
+					TextField ageText = new TextField();
+					TextField ssnText = new TextField();
 					TextField ccText = new TextField();
-					HBox ccTextHB = new HBox();
-					ccTextHB.setAlignment(Pos.CENTER_LEFT);
-					ccTextHB.getChildren().add(ccText);
-					insertDataGrid.add(ccTextHB, 4, 5);
-
 					Button enterInsertDataBtn = new Button("Enter Data");
-					HBox enterInsertDataBtnHB = new HBox();
-					enterInsertDataBtnHB.setAlignment(Pos.CENTER);
-					enterInsertDataBtnHB.getChildren().add(enterInsertDataBtn);
-					insertDataGrid.add(enterInsertDataBtnHB, 3, 6);
-
 					Button closeBtn = new Button("Close");
-					HBox closeBtnHB = new HBox();
-					closeBtnHB.setAlignment(Pos.CENTER);
-					closeBtnHB.getChildren().add(closeBtn);
-					insertDataGrid.add(closeBtnHB, 4, 6);
+
+					VBox labelVBox = new VBox();
+					VBox textFieldVBox = new VBox();
+					HBox buttonHBox = new HBox();
+
+					labelVBox.setAlignment(Pos.CENTER_RIGHT);
+					textFieldVBox.setAlignment(Pos.CENTER_LEFT);
+					buttonHBox.setAlignment(Pos.CENTER);
+
+					labelVBox.setSpacing(8);
+					buttonHBox.setSpacing(30);
+
+					labelVBox.getChildren().addAll(firstNameLabel,
+							lastNameLabel, ageLabel, ssnLabel, ccLabel);
+					textFieldVBox.getChildren().addAll(firstNameText,
+							lastNameText, ageText, ssnText, ccText);
+					buttonHBox.getChildren().addAll(enterInsertDataBtn,
+							closeBtn);
+
+					insertDataGrid.add(labelVBox, 3, 1);
+					insertDataGrid.add(textFieldVBox, 4, 1);
+					insertDataGrid.add(buttonHBox, 4, 6);
 
 					enterInsertDataBtn
 							.setOnAction(new EventHandler<ActionEvent>() {
@@ -157,14 +117,16 @@ public class Main extends Application {
 									String lastName = lastNameText.getText();
 									int age = Integer.parseInt(ageText
 											.getText());
-									long ssn = Long.parseLong(ssnText.getText());
-									long cc = Long.parseLong(ccText.getText());
+									String ssn = ssnText.getText();
+									String cc = ccText.getText();
 
-									Person personInfo = new Person(firstName,
-											lastName, age, ssn, cc);
 									try {
 
-										System.out.println(db
+										Person personInfo = new Person(
+												firstName, lastName, age, ssn,
+												cc);
+
+										System.out.println(database
 												.insertPerson(personInfo));
 
 										firstNameText.setText("");
@@ -175,6 +137,24 @@ public class Main extends Application {
 
 									} catch (SQLException e1) {
 										e1.printStackTrace();
+									} catch (InvalidSsnException e1) {
+										Alert ssnAlert = new Alert(
+												AlertType.INFORMATION);
+										ssnAlert.setTitle("Invalid Input");
+										ssnAlert.setHeaderText("Invalid Social Security Number"
+												+ "\nYour input: "
+												+ ssnText.getText());
+										ssnAlert.setContentText("Re-enter with atleast 9 digits");
+										ssnAlert.showAndWait();
+									} catch (InvalidCCException e1) {
+										Alert ccAlert = new Alert(
+												AlertType.INFORMATION);
+										ccAlert.setTitle("Invalid Input");
+										ccAlert.setHeaderText("Invalid Credit Card Info"
+												+ "\nYour input: "
+												+ ccText.getText());
+										ccAlert.setContentText("Re-enter with atleast 16 digits");
+										ccAlert.showAndWait();
 									}
 								}
 
@@ -197,8 +177,8 @@ public class Main extends Application {
 
 					GridPane deleteDataGrid = new GridPane();
 					deleteDataGrid.setAlignment(Pos.CENTER);
-					deleteDataGrid.setHgap(5.0);
-					deleteDataGrid.setVgap(5.0);
+					deleteDataGrid.setVgap(10);
+					deleteDataGrid.setHgap(5);
 
 					Scene deleteDataScene = new Scene(deleteDataGrid, 300, 250);
 					deleteDataStage.setScene(deleteDataScene);
@@ -206,45 +186,38 @@ public class Main extends Application {
 					deleteDataStage.show();
 
 					Text scenetitle = new Text("Enter the first and last name");
+					Label firstNameLabel = new Label("First Name:");
+					Label lastNameLabel = new Label("Last Name:");
+					TextField firstNameText = new TextField();
+					TextField lastNameText = new TextField();
+					Button enterDeleteDataBtn = new Button("Delete Data");
+					Button closeBtn = new Button("Close");
+
 					scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL,
 							12));
+
+					VBox labelVBox = new VBox();
+					VBox textFieldVBox = new VBox();
+					HBox buttonHBox = new HBox();
+
+					labelVBox.setAlignment(Pos.CENTER_RIGHT);
+					textFieldVBox.setAlignment(Pos.CENTER_LEFT);
+					buttonHBox.setAlignment(Pos.CENTER);
+
+					labelVBox.setSpacing(8);
+					buttonHBox.setSpacing(30);
+
+					labelVBox.getChildren().addAll(firstNameLabel,
+							lastNameLabel);
+					textFieldVBox.getChildren().addAll(firstNameText,
+							lastNameText);
+					buttonHBox.getChildren().addAll(enterDeleteDataBtn,
+							closeBtn);
+
 					deleteDataGrid.add(scenetitle, 4, 0);
-
-					Label firstNameLabel = new Label("First Name:");
-					HBox firstNameLabelHB = new HBox();
-					firstNameLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					firstNameLabelHB.getChildren().add(firstNameLabel);
-					deleteDataGrid.add(firstNameLabelHB, 3, 1);
-
-					TextField firstNameText = new TextField();
-					HBox firstNameTextHB = new HBox();
-					firstNameTextHB.setAlignment(Pos.CENTER_LEFT);
-					firstNameTextHB.getChildren().add(firstNameText);
-					deleteDataGrid.add(firstNameTextHB, 4, 1);
-
-					Label lastNameLabel = new Label("Last Name:");
-					HBox lastNameLabelHB = new HBox();
-					lastNameLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					lastNameLabelHB.getChildren().add(lastNameLabel);
-					deleteDataGrid.add(lastNameLabelHB, 3, 2);
-
-					TextField lastNameText = new TextField();
-					HBox lastNameTextHB = new HBox();
-					lastNameTextHB.setAlignment(Pos.CENTER_LEFT);
-					lastNameTextHB.getChildren().add(lastNameText);
-					deleteDataGrid.add(lastNameTextHB, 4, 2);
-
-					Button enterDeleteDataBtn = new Button("Delete Data");
-					HBox enterDeleteDataBtnHB = new HBox();
-					enterDeleteDataBtnHB.setAlignment(Pos.CENTER);
-					enterDeleteDataBtnHB.getChildren().add(enterDeleteDataBtn);
-					deleteDataGrid.add(enterDeleteDataBtnHB, 3, 3);
-
-					Button closeBtn = new Button("Close");
-					HBox closeBtnHB = new HBox();
-					closeBtnHB.setAlignment(Pos.CENTER);
-					closeBtnHB.getChildren().add(closeBtn);
-					deleteDataGrid.add(closeBtnHB, 4, 3);
+					deleteDataGrid.add(labelVBox, 3, 1);
+					deleteDataGrid.add(textFieldVBox, 4, 1);
+					deleteDataGrid.add(buttonHBox, 4, 3);
 
 					enterDeleteDataBtn
 							.setOnAction(new EventHandler<ActionEvent>() {
@@ -254,12 +227,17 @@ public class Main extends Application {
 									String lastName = lastNameText.getText();
 
 									try {
-										System.out.println(db.deletePerson(
-												firstName, lastName));
+										System.out.println(database
+												.deletePerson(firstName,
+														lastName));
 
 										firstNameText.setText("");
 										lastNameText.setText("");
 									} catch (SQLException e1) {
+										e1.printStackTrace();
+									} catch (InvalidSsnException e1) {
+										e1.printStackTrace();
+									} catch (InvalidCCException e1) {
 										e1.printStackTrace();
 									}
 
@@ -278,13 +256,86 @@ public class Main extends Application {
 			});
 
 			searchAllBtn.setOnAction(new EventHandler<ActionEvent>() {
+
 				public void handle(ActionEvent e) {
 
+					Stage searchAllStage = new Stage();
+					searchAllStage.setTitle("Database");
+
+					GridPane searchAllGrid = new GridPane();
+					searchAllGrid.setAlignment(Pos.CENTER);
+					searchAllGrid.setHgap(5.0);
+					searchAllGrid.setVgap(5.0);
+
+					Scene searchAllScene = new Scene(searchAllGrid, 950, 450);
+					searchAllStage.setScene(searchAllScene);
+
+					searchAllStage.show();
+
+					Label firstNameLabel = new Label("First Name");
+					Label lastNameLabel = new Label("Last Name");
+					Label ageLabel = new Label("Age");
+					Label ssnLabel = new Label("Social Security");
+					Label ccLabel = new Label("Credit Card");
+					ListView<Person> firstNameListView = new ListView<Person>();
+					ListView<Person> lastNameListView = new ListView<Person>();
+					ListView<Person> ageListView = new ListView<Person>();
+					ListView<Person> ssnListView = new ListView<Person>();
+					ListView<Person> ccListView = new ListView<Person>();
+
+					HBox firstNameLabelHBox = new HBox();
+					HBox lastNameLabelHBox = new HBox();
+					HBox ageLabelHBox = new HBox();
+					HBox ssnLabelHBox = new HBox();
+					HBox ccLabelHBox = new HBox();
+
+					firstNameLabelHBox.setAlignment(Pos.CENTER);
+					lastNameLabelHBox.setAlignment(Pos.CENTER);
+					ageLabelHBox.setAlignment(Pos.CENTER);
+					ssnLabelHBox.setAlignment(Pos.CENTER);
+					ccLabelHBox.setAlignment(Pos.CENTER);
+
+					firstNameLabelHBox.getChildren().add(firstNameLabel);
+					lastNameLabelHBox.getChildren().add(lastNameLabel);
+					ageLabelHBox.getChildren().add(ageLabel);
+					ssnLabelHBox.getChildren().add(ssnLabel);
+					ccLabelHBox.getChildren().add(ccLabel);
+
+					searchAllGrid.add(firstNameLabelHBox, 1, 1);
+					searchAllGrid.add(lastNameLabelHBox, 2, 1);
+					searchAllGrid.add(ageLabelHBox, 3, 1);
+					searchAllGrid.add(ssnLabelHBox, 4, 1);
+					searchAllGrid.add(ccLabelHBox, 5, 1);
+					searchAllGrid.add(firstNameListView, 1, 2);
+					searchAllGrid.add(lastNameListView, 2, 2);
+					searchAllGrid.add(ageListView, 3, 2);
+					searchAllGrid.add(ssnListView, 4, 2);
+					searchAllGrid.add(ccListView, 5, 2);
+
+					ObservableList<Person> firstNameOL;
+					ObservableList<Person> lastNameOL;
+					ObservableList<Person> ageOL;
+					ObservableList<Person> ssnOL;
+					ObservableList<Person> ccOL;
 					try {
-						System.out.println(db.findAllPeople());
-						System.out.println("\n\n\n");
-					} catch (Exception e1) {
-						e1.printStackTrace();
+						firstNameOL = FXCollections.observableList(database
+								.findAllPeopleByFirstName());
+						lastNameOL = FXCollections.observableList(database
+								.findAllPeopleByLastName());
+						ageOL = FXCollections.observableList(database
+								.findAllPeopleByAge());
+						ssnOL = FXCollections.observableList(database
+								.findAllPeopleBySsn());
+						ccOL = FXCollections.observableList(database
+								.findAllPeopleByCC());
+
+						firstNameListView.setItems(firstNameOL);
+						lastNameListView.setItems(lastNameOL);
+						ageListView.setItems(ageOL);
+						ssnListView.setItems(ssnOL);
+						ccListView.setItems(ccOL);
+					} catch (Exception e2) {
+						e2.printStackTrace();
 					}
 
 				}
@@ -307,33 +358,32 @@ public class Main extends Application {
 					findPersonStage.show();
 
 					Text scenetitle = new Text("Enter the last name");
+					Label lastNameLabel = new Label("Last Name:");
+					TextField lastNameText = new TextField();
+					Button enterFindPersonBtn = new Button("Find Data");
+					Button closeBtn = new Button("Close");
+
 					scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL,
 							12));
-					findPersonGrid.add(scenetitle, 4, 0);
 
-					Label lastNameLabel = new Label("Last Name:");
 					HBox lastNameLabelHB = new HBox();
-					lastNameLabelHB.setAlignment(Pos.CENTER_RIGHT);
-					lastNameLabelHB.getChildren().add(lastNameLabel);
-					findPersonGrid.add(lastNameLabelHB, 3, 2);
-
-					TextField lastNameText = new TextField();
 					HBox lastNameTextHB = new HBox();
+					HBox buttonHBox = new HBox();
+
+					buttonHBox.setSpacing(35);
+
+					lastNameLabelHB.setAlignment(Pos.CENTER_RIGHT);
 					lastNameTextHB.setAlignment(Pos.CENTER_LEFT);
+
+					lastNameLabelHB.getChildren().add(lastNameLabel);
 					lastNameTextHB.getChildren().add(lastNameText);
+					buttonHBox.getChildren().addAll(enterFindPersonBtn,
+							closeBtn);
+
+					findPersonGrid.add(scenetitle, 4, 0);
+					findPersonGrid.add(lastNameLabelHB, 3, 2);
 					findPersonGrid.add(lastNameTextHB, 4, 2);
-
-					Button enterFindPersonBtn = new Button("Find Data");
-					HBox enterFindPersonBtnHB = new HBox();
-					enterFindPersonBtnHB.setAlignment(Pos.CENTER);
-					enterFindPersonBtnHB.getChildren().add(enterFindPersonBtn);
-					findPersonGrid.add(enterFindPersonBtnHB, 3, 3);
-
-					Button closeBtn = new Button("Close");
-					HBox closeBtnHB = new HBox();
-					closeBtnHB.setAlignment(Pos.CENTER);
-					closeBtnHB.getChildren().add(closeBtn);
-					findPersonGrid.add(closeBtnHB, 4, 3);
+					findPersonGrid.add(buttonHBox, 4, 3);
 
 					enterFindPersonBtn
 							.setOnAction(new EventHandler<ActionEvent>() {
@@ -342,9 +392,13 @@ public class Main extends Application {
 									String lastName = lastNameText.getText();
 
 									try {
-										System.out.println(db
+										System.out.println(database
 												.findPersonByLastName(lastName));
 									} catch (SQLException e1) {
+										e1.printStackTrace();
+									} catch (InvalidSsnException e1) {
+										e1.printStackTrace();
+									} catch (InvalidCCException e1) {
 										e1.printStackTrace();
 									}
 								}
